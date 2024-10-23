87170ac826cde68a92199bf8dbabc97548f02e25
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-22 22:43:19 -0400

    add some bad physics

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Lightformer, useGLTF } from "@react-three/drei";
-import { FC } from "react";
+import { useThree } from "@react-three/fiber";
+import { BallCollider, CuboidCollider, RigidBody } from "@react-three/rapier";
+import { FC, useCallback, useRef } from "react";
 import * as THREE from "three";
 import { GLTF } from "three-stdlib";
 
@@ -16,43 +18,83 @@ type GLTFResult = GLTF & {
 
 export const Cube: FC = (props: JSX.IntrinsicElements["group"]) => {
   const { nodes, materials } = useGLTF("/models/cube.glb") as GLTFResult;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const api = useRef<any>();
+  const { viewport } = useThree();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onCollisionEnter = useCallback((state: any) => {
+    state.api.reset();
+    api.current.setTranslation({ x: 0, y: 4, z: 0 });
+    api.current.setLinvel({ x: 0, y: 4, z: 0 });
+  }, []);
 
   return (
     <group {...props} dispose={null}>
-      <mesh
-        geometry={nodes.cube.geometry}
-        material={materials["Satin Glass"]}
-        scale={0.991}
-      />
-      <hemisphereLight intensity={0.5} />
-      <Lightformer
-        color="blue"
-        intensity={1}
-        scale={[1.8, 0.2, 1]}
-        position={[0, 0.7, 0.85]}
-        onUpdate={(self) => self.lookAt(0, 0, 0)}
-      />
-      <Lightformer
-        color="blue"
-        intensity={1}
-        scale={[1.8, 0.2, 1]}
-        position={[0, 0.7, -0.85]}
-        onUpdate={(self) => self.lookAt(0, 0, 0)}
-      />
-      <Lightformer
-        color="blue"
-        intensity={1}
-        scale={[1.8, 0.2, 1]}
-        position={[0, -0.7, 0.85]}
-        onUpdate={(self) => self.lookAt(0, 0, 0)}
-      />
-      <Lightformer
-        color="blue"
-        intensity={1}
-        scale={[1.8, 0.2, 1]}
-        position={[0, 0 - 0.7, -0.85]}
-        onUpdate={(self) => self.lookAt(0, 0, 0)}
-      />
+      <RigidBody
+        ccd
+        ref={api}
+        angularDamping={0.8}
+        restitution={1}
+        canSleep={false}
+        colliders={false}
+        enabledTranslations={[true, true, false]}
+      >
+        <BallCollider args={[0.5]} />
+        <mesh
+          geometry={nodes.cube.geometry}
+          material={materials["Satin Glass"]}
+          scale={0.991}
+        />
+        <hemisphereLight intensity={0.5} />
+        <Lightformer
+          color="blue"
+          intensity={1}
+          scale={[1.8, 0.2, 1]}
+          position={[0, 0.7, 0.85]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+        <Lightformer
+          color="blue"
+          intensity={1}
+          scale={[1.8, 0.2, 1]}
+          position={[0, 0.7, -0.85]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+        <Lightformer
+          color="blue"
+          intensity={1}
+          scale={[1.8, 0.2, 1]}
+          position={[0, -0.7, 0.85]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+        <Lightformer
+          color="blue"
+          intensity={1}
+          scale={[1.8, 0.2, 1]}
+          position={[0, 0 - 0.7, -0.85]}
+          onUpdate={(self) => self.lookAt(0, 0, 0)}
+        />
+      </RigidBody>
+
+      <RigidBody
+        type="fixed"
+        colliders={false}
+        position={[0, -viewport.height * 2, 0]}
+        restitution={2.1}
+        onCollisionEnter={onCollisionEnter}
+      >
+        <CuboidCollider args={[1000, 2, 1000]} />
+      </RigidBody>
+      <RigidBody
+        type="fixed"
+        colliders={false}
+        position={[0, viewport.height * 4, 0]}
+        restitution={2.1}
+        onCollisionEnter={onCollisionEnter}
+      >
+        <CuboidCollider args={[1000, 2, 1000]} />
+      </RigidBody>
     </group>
   );
 };
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -2,11 +2,12 @@
 
 import { CameraControls, Stars } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
+import { Physics } from "@react-three/rapier";
+import { FC, Suspense } from "react";
 import { Cube } from "./Cube";
 import { Ring } from "./Ring";
 
-export const Scene: React.FC = () => {
+export const Scene: FC = () => {
   return (
     <Canvas
       camera={{
@@ -14,62 +15,66 @@ export const Scene: React.FC = () => {
         fov: 5,
       }}
     >
-      <Stars />
-      <ambientLight intensity={Math.PI / 1.5} />
-      <spotLight
-        position={[50, 50, 50]}
-        angle={0.15}
-        penumbra={1}
-        color="#566EB1"
-        decay={0}
-        intensity={Math.PI * 10}
-      />
-      <spotLight
-        position={[-50, 50, 50]}
-        angle={0.15}
-        penumbra={1}
-        color="#7E91C6"
-        decay={0}
-        intensity={Math.PI * 10}
-      />
-      <pointLight
-        position={[-10, -10, -10]}
-        color="#7E91C6"
-        decay={0.3}
-        intensity={Math.PI}
-      />
-      <pointLight
-        position={[10, -10, -10]}
-        color="#566EB1"
-        decay={0.3}
-        intensity={Math.PI}
-      />
-      <pointLight
-        position={[-10, 10, -10]}
-        color="#566EB1"
-        decay={0.3}
-        intensity={Math.PI}
-      />
-      <pointLight
-        position={[-10, -50, -30]}
-        color="#7E91C6"
-        decay={0.3}
-        intensity={Math.PI}
-      />
-      <group rotation={[0, 1, 0]}>
-        <Ring />
-        <group>
-          <Cube />
-          <spotLight
-            position={[0, -1, 0]}
-            angle={1}
-            color="#7E91C6"
-            decay={0}
-            intensity={Math.PI * 1000}
-          />
+      <Suspense fallback={null}>
+        <Stars />
+        <ambientLight intensity={Math.PI / 1.5} />
+        <spotLight
+          position={[50, 50, 50]}
+          angle={0.15}
+          penumbra={1}
+          color="#566EB1"
+          decay={0}
+          intensity={Math.PI * 10}
+        />
+        <spotLight
+          position={[-50, 50, 50]}
+          angle={0.15}
+          penumbra={1}
+          color="#7E91C6"
+          decay={0}
+          intensity={Math.PI * 10}
+        />
+        <pointLight
+          position={[-10, -10, -10]}
+          color="#7E91C6"
+          decay={0.3}
+          intensity={Math.PI}
+        />
+        <pointLight
+          position={[10, -10, -10]}
+          color="#566EB1"
+          decay={0.3}
+          intensity={Math.PI}
+        />
+        <pointLight
+          position={[-10, 10, -10]}
+          color="#566EB1"
+          decay={0.3}
+          intensity={Math.PI}
+        />
+        <pointLight
+          position={[-10, -50, -30]}
+          color="#7E91C6"
+          decay={0.3}
+          intensity={Math.PI}
+        />
+        <group rotation={[0, 1, 0]}>
+          <Ring />
+          <Physics gravity={[0, -40, 0]} timeStep={1}>
+            <group>
+              <Cube />
+              <spotLight
+                position={[0, -1, 0]}
+                angle={1}
+                color="#7E91C6"
+                decay={0}
+                intensity={Math.PI * 1000}
+              />
+            </group>
+          </Physics>
         </group>
-      </group>
-      <CameraControls />
+        <CameraControls />
+      </Suspense>
     </Canvas>
   );
 };
