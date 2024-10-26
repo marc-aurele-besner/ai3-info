753903676b9bdb15906e5b22a561e3356e1018a2
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-26 12:26:44 -0400

    update and fix dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["three"],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
