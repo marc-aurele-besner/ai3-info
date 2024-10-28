7c048be439ec8cffbddea240b78a49eff3e81f91
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2024-10-28 08:13:41 -0400

    add camera path

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import { CameraControls, Stars } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React from "react";
+import { MotionPathControls, Stars, useMotion } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { FC, useRef } from "react";
+import { CubicBezierCurve3, Group, Object3D, Vector3 } from "three";
 import { Cube } from "./Cube";
 import { Ring } from "./Ring";
 
-export const Scene: React.FC = () => {
+export const Scene: FC = () => {
+  const poi = useRef<Group>(new Group());
+
   return (
     <Canvas
       camera={{
-        position: [100, 100, 60],
+        position: [100, 90, 60],
         fov: 5,
       }}
     >
@@ -80,7 +83,7 @@ export const Scene: React.FC = () => {
       />
 
       {/* Rotate Ring Model */}
-      <group rotation={[0, 1, 0]}>
+      <group rotation={[0, 1, 0]} ref={poi}>
         {/* Ring Model */}
         <Ring />
 
@@ -195,7 +198,50 @@ export const Scene: React.FC = () => {
         {/* The Cube */}
         <Cube />
       </group>
-      <CameraControls />
+      <MotionPathControls
+        focus={poi}
+        damping={0.25}
+        focusDamping={0.25}
+        smooth={true}
+      >
+        <CameraPath />
+        <Loop />
+      </MotionPathControls>
     </Canvas>
   );
+};
+
+export const CameraPath: FC = () => {
+  return (
+    <>
+      {[
+        [
+          new Vector3(100, 90, 60),
+          new Vector3(100, 80, 60),
+          new Vector3(100, 70, 60),
+          new Vector3(100, 60, 60),
+          new Vector3(100, 50, 60),
+          new Vector3(100, 40, 60),
+          new Vector3(100, 30, 60),
+        ],
+        [
+          new Vector3(100, 30, 60),
+          new Vector3(100, 40, 60),
+          new Vector3(100, 50, 60),
+          new Vector3(100, 60, 60),
+          new Vector3(100, 70, 60),
+          new Vector3(100, 80, 60),
+          new Vector3(100, 90, 60),
+        ],
+      ].map(([v0, v1, v2, v3], index) => (
+        <primitive key={index} object={new CubicBezierCurve3(v0, v1, v2, v3)} />
+      ))}
+    </>
+  );
+};
+
+const Loop: FC<{ factor?: number }> = ({ factor = 0.15 }) => {
+  const motion = useMotion();
+  useFrame((state, delta) => (motion.current += Math.min(0.1, delta) * factor));
+  return null;
 };
