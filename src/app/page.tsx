21243c5614790581920327091db9be109db156dd
Marc-Aurele Besner <marcaurele.desk@gmail.com>
2024-11-06 15:25:43 -0500

    switch to display mainnet info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen p-8 bg-black text-white">
       <h1 className="text-4xl font-bold mb-4">Autonomys Network Info</h1>
-      <h2 className="text-4xl font-bold mb-4">{NetworkName.TAURUS}</h2>
+      <h2 className="text-4xl font-bold mb-4">{NetworkName.MAINNET}</h2>
       <Scene />
     </div>
   );
